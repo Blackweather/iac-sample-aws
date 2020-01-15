89117e36b4c2c086dd47d3c697282b7db09ee017
Blackweather <szczepanski.karol0@gmail.com>
2020-01-15 20:29:29 +0100

    Add IAM profile for CloudWatch logging

--- infrastructure/aws/compute/asg.tf
+++ infrastructure/aws/compute/asg.tf
@@ -1,8 +1,11 @@
 resource "aws_launch_configuration" "web_asg_lc" {
-  image_id = "${lookup(var.aws_amis, var.region)}"
-  instance_type = "${var.instance_type}"
+  image_id        = "${lookup(var.aws_amis, var.region)}"
+  instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.instance.id}"]
-  user_data = "${file("compute/userdata.sh")}"
+  user_data       = "${file("compute/userdata.sh")}"
+
+  iam_instance_profile = "${aws_iam_instance_profile.CloudWatchAgentServerRole-instanceprofile.name}"
+
   lifecycle {
     # prevent timeout
     create_before_destroy = true
@@ -11,20 +14,20 @@ resource "aws_launch_configuration" "web_asg_lc" {
 
 
 resource "aws_autoscaling_group" "web_asg" {
-  name = "web-asg"
+  name                 = "web-asg"
   launch_configuration = "${aws_launch_configuration.web_asg_lc.id}"
 
   vpc_zone_identifier = "${var.private_subnets}"
-  target_group_arns = ["${aws_alb_target_group.web_alb_tg.arn}"]
+  target_group_arns   = ["${aws_alb_target_group.web_alb_tg.arn}"]
 
   desired_capacity = "${var.instance_count}"
-  max_size = "${var.max_instance_count}"
-  min_size = "${var.min_instance_count}"
+  max_size         = "${var.max_instance_count}"
+  min_size         = "${var.min_instance_count}"
 
   health_check_type = "ELB"
   tag {
-    key = "Name"
-    value = "web-asg"
+    key                 = "Name"
+    value               = "web-asg"
     propagate_at_launch = true
   }
 
--- infrastructure/aws/compute/userdata.sh
+++ infrastructure/aws/compute/userdata.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -v
+# download cloudwatch agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+usermod -aG adm cwagent
+
+# setup configuration for cloudwatch
+
+# run cloudwatch agent
+
+# download and start nginx
 apt-get update -y
 apt-get install -y nginx
 systemctl enable nginx
 systemctl start nginx
-#echo "You reached the server" > /var/www/html/index.html
